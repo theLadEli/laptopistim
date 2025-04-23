391a6d57257c6148338cc92b5e7584be2c1eacf9
Eli <coheneli98@gmail.com>
2025-04-24 02:31:29 +0300

    Updated base domain to sort CORS issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://laptopistim.com/',
+  base: '/',
+  // base: 'https://laptopistim.com/',
   plugins: [react()],
   server: {
     port: 5173,
