b6816b8fb31557d9c72d88c0f486cab55192f32a
Eli <coheneli98@gmail.com>
2025-04-23 22:18:19 +0300

    Added PostHog and Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from "./AuthContext";
+import { PostHogProvider } from 'posthog-js/react';
+
+const options = {
+  api_host: "https://eu.i.posthog.com",
+  debug: import.meta.env.MODE === "development"
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <App />
+    <PostHogProvider 
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </AuthProvider>
 );
